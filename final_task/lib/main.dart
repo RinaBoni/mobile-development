74ea9bb0cca65778d6cb6978dc3310ca06e85d96
RinaBoni <brisovae@mail.ru>
2023-05-18 18:20:57 +0900

    готово на 20%
    Дизайн входа и регистрации, набросок главной страницы, набросок меню, жалкая попытка в темы

--- final_task/lib/main.dart
+++ final_task/lib/main.dart
@@ -1,15 +1,30 @@
+import 'package:final_task/theme/theme_constants.dart';
+import 'package:final_task/theme/theme_manager.dart';
 import 'package:flutter/material.dart';
-import './login_form.dart';
-import './anonymous_routes.dart';
+import './signin.dart';
+import './signup.dart';
+import './home_screen.dart';
+import './NavBar.dart';
+import './theme/theme_constants.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: '/',
-    routes: {
-      '/': (context) => HomeScreen(),
-      '/login_form': (context) => DetailScreen(),//const LoginForm(),
-    },
-  ),
-  );  //rumApp
+void main() => runApp(MyApp());
+
+//ThemeManager _themeManager = ThemeManager();
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //theme: ThemeClass.darkTheme,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/login_form': (context) => LoginPage(),
+        '/registration': (context) => SignUpPage(),
+      },
+    );
+  }
 }
+
--- final_task/lib/utils/theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-ThemeData basicTheme() => ThemeData(
-  buttonTheme: ButtonThemeData(
-    buttonColor: Colors.blueAccent,
-    textTheme: ButtonTextTheme.accent,
-  ), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
-);
